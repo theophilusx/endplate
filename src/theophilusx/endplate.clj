f9b82e872b3a121a2537c61d336b72c6b6cf0833
Tim Cross <theophilusx@gmail.com>
2024-08-26 12:37:04 +1000

    Add basic template parsing and tests

--- src/theophilusx/endplate.clj
+++ src/theophilusx/endplate.clj
@@ -1,6 +1,41 @@
-(ns theophilusx.endplate)
+(ns theophilusx.endplate
+  (:require [clojure.tools.logging :as log]
+            [clojure.edn :as edn]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (prn x "Hello, World!"))
+(def template-dir 
+  "The path to the directory containing template files.
+  Default is the direcvtory resources/templates."
+  (atom "resources/templates"))
+
+(defn parse-template
+  ([template]
+   (parse-template template {}))
+  ([template template-vars]
+   (try
+     (let [vars    (atom template-vars)
+           data    (slurp (str @template-dir "/" template))
+           defvars (fn [v-map]
+                     (reset! vars (merge v-map @vars)))
+           getv    (fn [v-name]
+                     (let [v (get @vars v-name (str "MISSING_TEMPLATE_VALUE_" v-name))]
+                       (try 
+                         (cond
+                           (and
+                            (vector? v)
+                            (keyword? (first v))) v
+                           (list? v)              (eval v)
+                           :else                  (str v))
+                         (catch Exception e
+                           (log/error e (ex-message e))
+                           (throw e)))))
+           readers {:readers {'endplate/def defvars
+                              'endplate/val getv}}
+           edn     (edn/read-string readers data)]
+       (:template edn))
+     (catch Exception e
+       (let [msg (str "parse-template: " (ex-message e))]
+         (log/error e msg)
+         (throw (ex-info msg {:template template :vars template-vars} e)))))))
+
+
+
--- test/theophilusx/endplate_test.clj
+++ test/theophilusx/endplate_test.clj
@@ -1,7 +1,17 @@
 (ns theophilusx.endplate-test
-  (:require [clojure.test :refer :all]
-            [theophilusx.endplate :refer :all]))
+  (:require [clojure.test :as t]
+            [theophilusx.endplate :as sut]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(t/deftest parse-template
+  (t/testing "Cleanly handle missing template"
+    (t/is (thrown-with-msg? Exception #"parse-template: resources/templates/not-exist.edn \(No such file or directory\)"
+                            (sut/parse-template "not-exist.edn"))))
+  (t/testing "String template"
+    (t/is (string? (sut/parse-template "string.edn"))))
+  (t/testing "Map template"
+    (t/is (map? (sut/parse-template "map.edn"))))
+  (t/testing "Vector template"
+    (t/is (vector? (sut/parse-template "vector.edn")))))
+
+
+
