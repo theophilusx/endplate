cfd114e2a5524c9ca52c3fa870355ceff4e485ad
Tim Cross <theophilusx@gmail.com>
2024-09-10 10:04:14 +1000

    Added basic template tests

--- src/theophilusx/endplate.clj
+++ src/theophilusx/endplate.clj
@@ -1,41 +1,52 @@
 (ns theophilusx.endplate
   (:require [clojure.tools.logging :as log]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [clojure.string :as s]))
 
 (def template-dir 
   "The path to the directory containing template files.
   Default is the direcvtory resources/templates."
   (atom "resources/templates"))
 
-(defn parse-template
-  ([template]
-   (parse-template template {}))
-  ([template template-vars]
-   (try
-     (let [vars    (atom template-vars)
-           data    (slurp (str @template-dir "/" template))
-           defvars (fn [v-map]
-                     (reset! vars (merge v-map @vars)))
-           getv    (fn [v-name]
-                     (let [v (get @vars v-name (str "MISSING_TEMPLATE_VALUE_" v-name))]
-                       (try 
-                         (cond
-                           (and
-                            (vector? v)
-                            (keyword? (first v))) v
-                           (list? v)              (eval v)
-                           :else                  (str v))
-                         (catch Exception e
-                           (log/error e (ex-message e))
-                           (throw e)))))
-           readers {:readers {'endplate/def defvars
-                              'endplate/val getv}}
-           edn     (edn/read-string readers data)]
-       (:template edn))
-     (catch Exception e
-       (let [msg (str "parse-template: " (ex-message e))]
-         (log/error e msg)
-         (throw (ex-info msg {:template template :vars template-vars} e)))))))
+(defn parse-template [template-file & {:keys [template-vars hiccup-vectors return-string
+                                              list-eval]
+                                       :or   {template-vars  {}
+                                              hiccup-vectors false
+                                              list-eval      true
+                                              return-string  false}}]
+  (try
+    (let [vars    (atom template-vars)
+          data    (slurp (str @template-dir "/" template-file))
+          defvars (fn [v-map]
+                    (reset! vars (merge v-map @vars)))
+          getv    (fn [v-name]
+                    (let [v (get @vars v-name (str "MISSING_TEMPLATE_VALUE_" v-name))]
+                      (try
+                        (cond
+                          (and (vector? v)
+                               hiccup-vectors) (if (keyword? (first v))
+                                                 v
+                                                 (str v))
+                          (list? v)            (if list-eval
+                                                 (eval v)
+                                                 v)
+                          :else                v)
+                        (catch Exception e
+                          (log/error e (ex-message e))
+                          (throw e)))))
+          readers {:readers {'endplate/def defvars
+                             'endplate/val getv}}
+          edn     (edn/read-string readers data)]
+      (if return-string
+        (s/join " " (:template edn))
+        (:template edn)))
+    (catch Exception e
+      (let [msg (str "parse-template: " (ex-message e))]
+        (log/error e msg)
+        (throw (ex-info msg {:template template-file :vars template-vars} e))))))
+
+
+
 
 
 
--- test/theophilusx/endplate_test.clj
+++ test/theophilusx/endplate_test.clj
@@ -7,11 +7,41 @@
     (t/is (thrown-with-msg? Exception #"parse-template: resources/templates/not-exist.edn \(No such file or directory\)"
                             (sut/parse-template "not-exist.edn"))))
   (t/testing "String template"
-    (t/is (string? (sut/parse-template "string.edn"))))
+    (t/is (string? (sut/parse-template "string.edn" {:return-string true}))))
   (t/testing "Map template"
     (t/is (map? (sut/parse-template "map.edn"))))
   (t/testing "Vector template"
     (t/is (vector? (sut/parse-template "vector.edn")))))
 
+(t/deftest parse-template-with-vars
+  (t/testing "Basic string template with bundled variable"
+    (let [rslt (sut/parse-template "string-with-var.edn" {:return-string true})]
+      (t/is (string? rslt))
+      (t/is (= "Hello from local template var" rslt))))
+  (t/testing "Basic string template with user space variable"
+    (let [rslt (sut/parse-template "string-with-var.edn" {:template-vars {:from-who "user template var"}
+                                                          :return-string true})]
+      (t/is  (string? rslt))
+      (t/is (= "Hello from user template var" rslt))))
+  (t/testing "Basic map template with bundled variables"
+    (let [rslt (sut/parse-template "map-with-var.edn")]
+      (t/is (map? rslt))
+      (t/is (= (:key1 rslt) 1))
+      (t/is (= (:sk1 rslt) "This is some variable value"))
+      (t/is (= (:sk2 rslt) "This is some other variable"))))
+  (t/testing "Basic map template with bundled and user variables"
+    (let [rslt (sut/parse-template "map-with-var.edn" {:template-vars {:some-key2 "Some user supplied var"}})]
+      (t/is (map? rslt))
+      (t/is (= (:key1 rslt) 1))
+      (t/is (= (:sk1 rslt) "This is some variable value"))
+      (t/is (= (:sk2 rslt) "Some user supplied var"))))
+  (t/testing "Basic vector template with bundled variables"
+    (let [rslt (sut/parse-template "vector-with-var.edn")]
+      (t/is (vector? rslt))
+      (t/is (= rslt [:a 2 "3" :f [9 8 7]]))))
+  (t/testing "Basic vector template with bundled and user space variables"
+    (let [rslt (sut/parse-template "vector-with-var.edn" {:template-vars {:some-key1 "fred"}})]
+      (t/is (vector? rslt))
+      (t/is (= rslt [:a 2 "3" "fred" [9 8 7]])))))
 
 
